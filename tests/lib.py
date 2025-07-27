a1a6c8f02242badb4b9864ba812a8e7cc4909aaa
Albert Hopkins <marduk@letterboxes.org>
2025-07-27 15:21:02 -0500

    tests: fixtures: add "clear" kwarg to environ

--- tests/lib.py
+++ tests/lib.py
@@ -9,9 +9,9 @@ from unittest_fixtures import FixtureContext, Fixtures, fixture
 
 @fixture()
 def environ(
-    _fixtures: Fixtures, *, environ: Mapping[str, str] | None = None
+    _fixtures: Fixtures, *, environ: Mapping[str, str] | None = None, clear: bool = True
 ) -> FixtureContext[MutableMapping[str, str]]:
-    with mock.patch.dict(os.environ, clear=True):
+    with mock.patch.dict(os.environ, clear=clear):
         os.environ.update(environ or {})
         yield os.environ
 
